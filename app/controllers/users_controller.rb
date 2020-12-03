8b916c3782bb13ca5c0aa9d2da727fe830e2ac29
coxcoa <73727547+maxtanabe@users.noreply.github.com>
2020-12-03 16:41:25 +0900

    修正１

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = Post.all.order("created_at DESC")
+    @posts = @user.posts
   end
 
   private
