e49b149ef1f42fb919c65352d721ed26d5816d8c
coxcoa <73727547+maxtanabe@users.noreply.github.com>
2020-12-26 15:33:50 +0900

    コメント機能(修正6)

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -12,12 +12,11 @@ consumer.subscriptions.create("MessageChannel", {
 
   received(data) {
     
-    if (data.user = data.current_user){
+    if (data.user == data.current_user){
       var de = `<a id="delete-btn", data-method="delete" href="/posts/${data.post.id}/comments/${data.id}"><button id="${data.id}">削除</button></a>` ;
     }else{
       var de = null ;
     };
-console.log(de)
     const html = `<div id="test-${data.id}">
                   <p><a href="/users/${data.user.id}">@${data.user.nickname}</a></p>
                   <span style="font-weight:bold;">${data.content.content}</span>
